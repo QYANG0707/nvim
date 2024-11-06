d2e522809fb3023affeab682b0edda14f7ab2f34
QYANG0707 <QYANG0707@163.com>
2024-11-07 03:22:35 +0800

    ~

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,4 +19,11 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("lspconfig").lua_ls.setup {
   capabilities = capabilities,
+
 }
+
+
+
+require("lspconfig").pyright.setup({
+    -- capabilities = capabilities, 
+})
